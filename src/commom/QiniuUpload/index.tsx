00ce736c5a9b8fc7c1f2ddcac8e84a6f9c818586
Y-qwq <yezijunqwq@gmail.com>
2019-12-06 23:23:14 +0800

    feat: 基于七牛云和antd Upload封装上传组件

---
+++ src/commom/QiniuUpload/index.tsx
@@ -0,0 +1,82 @@
+import React, { ReactNode, forwardRef, useCallback, useState } from "react";
+import { Upload } from "antd";
+import { QINIU_SERVER, getForceUploadToken } from "@/api";
+import { useSelector } from "react-redux";
+import { IState } from "@/redux/reducers";
+import {
+  RcFile,
+  UploadChangeParam,
+  UploadFile
+} from "antd/lib/upload/interface";
+
+export interface IQiniuUpload {
+  beforeUpload?: (
+    file: RcFile,
+    FileList: RcFile[]
+  ) => boolean | PromiseLike<void>;
+  onChange?: (info: UploadChangeParam<UploadFile<any>>) => void;
+  path?: string;
+  children?: ReactNode;
+  className?: string;
+  listType?: "picture" | "text" | "picture-card" | undefined;
+  token?: string;
+  forceUpload?: boolean;
+}
+const QiniuUpload = forwardRef(
+  (
+    {
+      beforeUpload,
+      onChange,
+      path,
+      children,
+      className,
+      listType,
+      token,
+      forceUpload
+    }: IQiniuUpload,
+    ref: any
+  ) => {
+    const { qiniu } = useSelector((state: IState) => state.user.info);
+    const [forceToken, setForceToken] = useState("");
+
+    const handleBeforeUpload = useCallback(
+      async (file: RcFile, FileList: RcFile[]) => {
+        // 获取强制上传token
+        if (forceUpload && path) {
+          const res = await getForceUploadToken(path);
+          res.data.type === "success" && setForceToken(res.data.token);
+        }
+        // 如果有自定义beforeUpload,则执行
+        if (beforeUpload) {
+          const res = beforeUpload(file, FileList);
+          if (!res) {
+            // async/await中执行reject的写法.
+            throw new Error("停止上传！");
+          }
+        }
+      },
+      [beforeUpload, forceUpload, path]
+    );
+
+    return (
+      <Upload
+        ref={ref}
+        className={className}
+        listType={listType}
+        accept="image/*"
+        showUploadList={false}
+        action={QINIU_SERVER}
+        beforeUpload={handleBeforeUpload}
+        onChange={onChange}
+        data={{
+          token: forceToken || token || (qiniu && qiniu.token),
+          key: path
+        }}
+      >
+        {children}
+      </Upload>
+    );
+  }
+);
+
+export default QiniuUpload;

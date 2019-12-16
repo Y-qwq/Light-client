311092d35d1a7b9b0df31ed536da997fcab8c026
Y-qwq <yezijunqwq@gmail.com>
2019-12-16 17:22:09 +0800

    feat: 完成ME模块上半部分UI

--- src/assets/MyIcon.ts
+++ src/assets/MyIcon.ts
@@ -1,7 +1,7 @@
 import { Icon } from "antd";
 
 const MyIcon = Icon.createFromIconfontCN({
-  scriptUrl: "//at.alicdn.com/t/font_1489077_4c1l3prq3j7.js" // 在 iconfont.cn 上生成
+  scriptUrl: "//at.alicdn.com/t/font_1489077_vt7zokf14d9.js" // 在 iconfont.cn 上生成
 });
 
 export default MyIcon;
--- src/pages/user/Me/index.tsx
+++ src/pages/user/Me/index.tsx
@@ -1,8 +1,38 @@
 import React from "react";
+import { Avatar } from "antd";
+import { QINIU_CLIENT } from "@/api";
+import { useSelector } from "react-redux";
+import { IState } from "@/redux/reducers";
+import MyIcon from "@/assets/MyIcon";
 import "./index.scss";
 
 const Me = () => {
-  return <h1>2</h1>;
+  const user = useSelector((state: IState) => state.user.info);
+
+  return (
+    <div className="me">
+      <div className="me-header">
+        <MyIcon type="setting" className="me-header-setting" />
+        <Avatar
+          size={64}
+          className="me-header-avatar"
+          src={`${QINIU_CLIENT}/avatar/${user._id}?h=${user.avatar}`}
+        />
+        <p className="me-header-name">{user.username}</p>
+        <p className="me-header-introduction">{user.introduction}</p>
+      </div>
+      <div className="me-collections">
+        <p className="me-collections-title">我的收藏</p>
+        <div className="me-collections-content">
+          <MyIcon type="eye" className="me-collections-icons" />
+          <MyIcon type="image" className="me-collections-icons" />
+          <MyIcon type="music" className="me-collections-icons" />
+          <MyIcon type="movie" className="me-collections-icons" />
+          <MyIcon type="fm" className="me-collections-icons" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Me;
